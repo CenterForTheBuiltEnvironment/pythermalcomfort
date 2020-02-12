52e5ff6ec2e680aadca4818bdfef428823a00e6f
FedericoTartarini <federicotartarini@gmail.com>
2020-02-12 10:48:26 +0800

    bumbed version

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ project = 'pythermalcomfort'
 year = '2019-2020'
 author = 'Federico Tartarini'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.2.2'
+version = release = '0.2.3'
 
 pygments_style = 'trac'
 templates_path = ['.']
