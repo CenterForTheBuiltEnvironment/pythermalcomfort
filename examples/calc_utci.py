8ad934a61fd9d8c3eabe3e4021fc25f36ff110d5
FedericoTartarini <federicotartarini@gmail.com>
2020-04-11 22:29:29 +0800

    utci example

---
+++ examples/calc_utci.py
@@ -0,0 +1,10 @@
+from pythermalcomfort.models import utci
+
+utci_val = utci(tdb=29, tr=30, v=1, rh=60)
+print(utci_val)
+
+utci_val = utci(tdb=29, tr=30, v=2, rh=60)
+print(utci_val)
+
+utci_val = utci(tdb=77, tr=77, v=6.56168, rh=60, units='IP')
+print(utci_val)
