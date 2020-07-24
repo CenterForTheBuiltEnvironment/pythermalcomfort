60813f6064e8d9013268c4d878a657c3d3219541
FedericoTartarini <federicotartarini@gmail.com>
2020-07-24 10:57:20 +0800

    * version 1.0.6

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -2,7 +2,7 @@
 Changelog
 =========
 
-1.0.5 (2020-07-24)
+1.0.6 (2020-07-24)
 ------------------
 
 * Minor speed improvement changed math.pow with **
--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ project = 'pythermalcomfort'
 year = '2019-2020'
 author = 'Federico Tartarini'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '1.0.5'
+version = release = '1.0.6'
 
 pygments_style = 'trac'
 templates_path = ['.']
