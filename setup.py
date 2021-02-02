ace863bba6b9472fb3b0b22dbac42b204a242da0
FedericoTartarini <federicotartarini@gmail.com>
2021-02-02 17:55:10 +0800

    Bump version: 1.3.4 → 1.3.5

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*names, **kwargs):
 
 setup(
     name='pythermalcomfort',
-    version='1.3.4',
+    version='1.3.5',
     license='MIT',
     description='Package to calculate several thermal comfort indices (e.g. PMV, PPD, SET, adaptive) and convert physical variables. Please cite us if you use this package: Tartarini, F., Schiavon, S., 2020. pythermalcomfort: A Python package for thermal comfort research. SoftwareX 12, 100578. https://doi.org/10.1016/j.softx.2020.100578',
     long_description='%s\n%s' % (
--- src/pythermalcomfort/__init__.py
+++ src/pythermalcomfort/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "__version__ = '1.3.4'"
+__version__ = "__version__ = '1.3.5'"
 
 from pythermalcomfort.models import *
