75b9dbade864d0f6b17e06050da6a5505f53fb57
FedericoTartarini <federicotartarini@gmail.com>
2021-03-11 17:29:15 +0800

    Bump version: 1.3.6 → 1.4.0

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ project = 'pythermalcomfort'
 year = '2019-2020'
 author = 'Federico Tartarini'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '1.3.6'
+version = release = '1.4.0'
 
 pygments_style = 'trac'
 templates_path = ['.']
--- src/pythermalcomfort/__init__.py
+++ src/pythermalcomfort/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "__version__ = '1.3.6'"
+__version__ = "__version__ = '1.4.0'"
 
 from pythermalcomfort.models import *
