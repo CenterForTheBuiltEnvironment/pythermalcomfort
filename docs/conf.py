41f57460145b4ca575ea923fb4c9c4b52b160575
FedericoTartarini <federicotartarini@gmail.com>
2021-03-29 12:13:00 +0800

    fix: specified dependencies version, preventing black to format version

--- docs/conf.py
+++ docs/conf.py
@@ -4,43 +4,51 @@ from __future__ import unicode_literals
 import os
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.coverage',
-    'sphinx.ext.doctest',
-    'sphinx.ext.extlinks',
-    'sphinx.ext.ifconfig',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.todo',
-    'sphinx.ext.viewcode',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.coverage",
+    "sphinx.ext.doctest",
+    "sphinx.ext.extlinks",
+    "sphinx.ext.ifconfig",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
 ]
-source_suffix = '.rst'
-master_doc = 'index'
-project = 'pythermalcomfort'
-year = '2019-2020'
-author = 'Federico Tartarini'
-copyright = '{0}, {1}'.format(year, author)
+source_suffix = ".rst"
+master_doc = "index"
+project = "pythermalcomfort"
+year = "2019-2020"
+author = "Federico Tartarini"
+copyright = "{0}, {1}".format(year, author)
+# fmt: off
 version = release = '1.4.0'
+# fmt: on
 
-pygments_style = 'trac'
-templates_path = ['.']
+pygments_style = "trac"
+templates_path = ["."]
 extlinks = {
-    'issue': ('https://github.com/CenterForTheBuiltEnvironment/pythermalcomfort/issues/%s', '#'),
-    'pr': ('https://github.com/CenterForTheBuiltEnvironment/pythermalcomfort/pull/%s', 'PR #'),
+    "issue": (
+        "https://github.com/CenterForTheBuiltEnvironment/pythermalcomfort/issues/%s",
+        "#",
+    ),
+    "pr": (
+        "https://github.com/CenterForTheBuiltEnvironment/pythermalcomfort/pull/%s",
+        "PR #",
+    ),
 }
 # on_rtd is whether we are on readthedocs.org
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+on_rtd = os.environ.get("READTHEDOCS", None) == "True"
 
 if not on_rtd:  # only set the theme if we're building docs locally
-    html_theme = 'sphinx_rtd_theme'
+    html_theme = "sphinx_rtd_theme"
 
 html_use_smartypants = True
-html_last_updated_fmt = '%b %d, %Y'
+html_last_updated_fmt = "%b %d, %Y"
 html_split_index = False
 html_sidebars = {
-    '**': ['searchbox.html', 'globaltoc.html', 'sourcelink.html'],
+    "**": ["searchbox.html", "globaltoc.html", "sourcelink.html"],
 }
-html_short_title = '%s-%s' % (project, version)
+html_short_title = "%s-%s" % (project, version)
 
 napoleon_use_ivar = True
 napoleon_use_rtype = False
--- setup.py
+++ setup.py
@@ -17,61 +17,71 @@ from setuptools import setup
 
 def read(*names, **kwargs):
     with io.open(
-        join(dirname(__file__), *names),
-        encoding=kwargs.get('encoding', 'utf8')
+        join(dirname(__file__), *names), encoding=kwargs.get("encoding", "utf8")
     ) as fh:
         return fh.read()
 
 
 setup(
+    # fmt: off
     name='pythermalcomfort',
     version='1.4.0',
-    license='MIT',
-    description='Package to calculate several thermal comfort indices (e.g. PMV, PPD, SET, adaptive) and convert physical variables. Please cite us if you use this package: Tartarini, F., Schiavon, S., 2020. pythermalcomfort: A Python package for thermal comfort research. SoftwareX 12, 100578. https://doi.org/10.1016/j.softx.2020.100578',
-    long_description='%s\n%s' % (
-        re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.rst')),
-        re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('CHANGELOG.rst'))
+    # fmt: on
+    license="MIT",
+    description="Package to calculate several thermal comfort indices (e.g. PMV, PPD, SET, adaptive) and convert physical variables. Please cite us if you use this package: Tartarini, F., Schiavon, S., 2020. pythermalcomfort: A Python package for thermal comfort research. SoftwareX 12, 100578. https://doi.org/10.1016/j.softx.2020.100578",
+    long_description="%s\n%s"
+    % (
+        re.compile("^.. start-badges.*^.. end-badges", re.M | re.S).sub(
+            "", read("README.rst")
+        ),
+        re.sub(":[a-z]+:`~?(.*?)`", r"``\1``", read("CHANGELOG.rst")),
     ),
-    author='Federico Tartarini',
-    author_email='cbecomforttool@gmail.com',
-    url='https://github.com/CenterForTheBuiltEnvironment/pythermalcomfort',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    author="Federico Tartarini",
+    author_email="cbecomforttool@gmail.com",
+    url="https://github.com/CenterForTheBuiltEnvironment/pythermalcomfort",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
     include_package_data=True,
     zip_safe=False,
     classifiers=[
         # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Education',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: Unix',
-        'Operating System :: POSIX',
-        'Operating System :: Microsoft :: Windows',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Utilities',
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Unix",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Utilities",
     ],
     project_urls={
-        'Documentation': 'https://pythermalcomfort.readthedocs.io/',
-        'Changelog': 'https://pythermalcomfort.readthedocs.io/en/latest/changelog.html',
-        'Issue Tracker': 'https://github.com/CenterForTheBuiltEnvironment/pythermalcomfort/issues',
+        "Documentation": "https://pythermalcomfort.readthedocs.io/",
+        "Changelog": "https://pythermalcomfort.readthedocs.io/en/latest/changelog.html",
+        "Issue Tracker": "https://github.com/CenterForTheBuiltEnvironment/pythermalcomfort/issues",
     },
     keywords=[
-        'thermal comfort', 'pmv', 'ppd', 'building design', 'compliance', 'thermal environment', 'buit environment'
+        "thermal comfort",
+        "pmv",
+        "ppd",
+        "building design",
+        "compliance",
+        "thermal environment",
+        "buit environment",
     ],
-    python_requires='>=3.6.*',
+    python_requires=">=3.6.*",
     install_requires=[
-        'scipy',
-        'numba'
+        "scipy==1.5.3",
+        "numba==0.50.1"
         # eg: 'aspectlib==1.1.1', 'six>=1.7',
     ],
     extras_require={
@@ -80,8 +90,8 @@ setup(
         #   ':python_version=="2.6"': ['argparse'],
     },
     entry_points={
-        'console_scripts': [
-            'pythermalcomfort = pythermalcomfort.cli:main',
+        "console_scripts": [
+            "pythermalcomfort = pythermalcomfort.cli:main",
         ]
     },
 )
