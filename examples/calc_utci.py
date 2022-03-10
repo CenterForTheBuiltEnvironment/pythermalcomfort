7c06b7987cc8710652549ebab4a1fc69d3978b05
Lars Buntemeyer <lars.buntemeyer@hzg.de>
2022-03-10 15:00:12 +0100

    update of numpy example

--- examples/calc_utci.py
+++ examples/calc_utci.py
@@ -12,18 +12,18 @@ print(utci_val)
 
 # numpy examples
 utci_val = utci(
-    tdb=np.array([29, 29]),
-    tr=np.array([30, 30]),
-    v=np.array([1, 2]),
-    rh=np.array([60, 60]),
+    tdb=np.array([29, 29, 25]),
+    tr=np.array([30, 30, 25]),
+    v=np.array([1, 2, 1]),
+    rh=np.array([60, 60, 50]),
 )
 print(utci_val)
 
 utci_val = utci(
-    tdb=np.array([29, 29]),
-    tr=np.array([30, 30]),
-    v=np.array([1, 2]),
-    rh=np.array([60, 60]),
+    tdb=np.array([29, 29, 25]),
+    tr=np.array([30, 30, 25]),
+    v=np.array([1, 2, 1]),
+    rh=np.array([60, 60, 50]),
     return_stress_category=True,
 )
 print(utci_val)
