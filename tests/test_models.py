acbe8697c7a1b56ed64213205b6e6e7f5e9659bf
Lars Buntemeyer <lars.buntemeyer@hzg.de>
2022-03-10 15:42:29 +0100

    black formatting for test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -1005,7 +1005,7 @@ def test_t_globe():
 
 
 def test_set_tmp():
-    """ Test the PMV function using the reference table from the ASHRAE 55 2020"""
+    """Test the PMV function using the reference table from the ASHRAE 55 2020"""
     for row in data_test_set:
         assert (
             abs(
@@ -1070,7 +1070,7 @@ def test_set_tmp():
 
 
 def test_pmv():
-    """ Test the PMV function using the reference table from the ASHRAE 55 2020"""
+    """Test the PMV function using the reference table from the ASHRAE 55 2020"""
     for row in data_test_pmv:
         assert (
             round(
@@ -1083,7 +1083,7 @@ def test_pmv():
 
 
 def test_pmv_ppd():
-    """ Test the PMV function using the reference table from the ASHRAE 55 2020"""
+    """Test the PMV function using the reference table from the ASHRAE 55 2020"""
     for row in data_test_pmv:
         assert (
             abs(
@@ -1348,16 +1348,14 @@ def test_ankle_draft():
 
 @pytest.fixture
 def data_test_adaptive_ashrae():
-    return (
-        [  # I have commented the lines of code that don't pass the test
-            {"tdb": 25, "tr": 27, "rh": 50, "v": 1, "return": {"utci": 25.2}},
-            {"tdb": 19, "tr": 24, "rh": 50, "v": 1, "return": {"utci": 20.0}},
-            {"tdb": 19, "tr": 14, "rh": 50, "v": 1, "return": {"utci": 16.8}},
-            {"tdb": 27, "tr": 22, "rh": 50, "v": 1, "return": {"utci": 25.5}},
-            {"tdb": 27, "tr": 22, "rh": 50, "v": 10, "return": {"utci": 20.0}},
-            {"tdb": 27, "tr": 22, "rh": 50, "v": 16, "return": {"utci": 15.8}},
-        ]
-    )
+    return [  # I have commented the lines of code that don't pass the test
+        {"tdb": 25, "tr": 27, "rh": 50, "v": 1, "return": {"utci": 25.2}},
+        {"tdb": 19, "tr": 24, "rh": 50, "v": 1, "return": {"utci": 20.0}},
+        {"tdb": 19, "tr": 14, "rh": 50, "v": 1, "return": {"utci": 16.8}},
+        {"tdb": 27, "tr": 22, "rh": 50, "v": 1, "return": {"utci": 25.5}},
+        {"tdb": 27, "tr": 22, "rh": 50, "v": 10, "return": {"utci": 20.0}},
+        {"tdb": 27, "tr": 22, "rh": 50, "v": 16, "return": {"utci": 15.8}},
+    ]
 
 
 def test_utci(data_test_adaptive_ashrae):
@@ -1389,7 +1387,10 @@ def test_utci_numpy(data_test_adaptive_ashrae):
     tr = np.array([27, 25])
     v = np.array([1, 1])
     rh = np.array([50, 50])
-    expect = {"utci": np.array([25.2, 24.6]), "stress_category": np.array(["no thermal stress", "no thermal stress"])}
+    expect = {
+        "utci": np.array([25.2, 24.6]),
+        "stress_category": np.array(["no thermal stress", "no thermal stress"]),
+    }
 
     result = utci(tdb, tr, v, rh, units="si", return_stress_category=True)
     np.testing.assert_equal(result["utci"], expect["utci"])
