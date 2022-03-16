75248885e019430804709fc718874d56a1c9c78b
Federico <federicotartarini@gmail.com>
2022-03-16 16:17:28 +0800

    Bump version: 1.10.0 → 1.11.0

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ project = "pythermalcomfort"
 year = "2019-2020"
 author = "Federico Tartarini"
 copyright = "{0}, {1}".format(year, author)
-version = release = "1.10.0"
+version = release = "1.11.0"
 
 pygments_style = "trac"
 templates_path = ["."]
