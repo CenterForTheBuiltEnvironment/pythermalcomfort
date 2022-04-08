37fa07a93c62d611dd6f7bd3f438eba98e1f7412
Federico <federicotartarini@gmail.com>
2022-04-08 12:11:27 +0800

    docs: changed a bit example pmv calculation

--- examples/calc_pmv_ppd.py
+++ examples/calc_pmv_ppd.py
@@ -6,7 +6,7 @@ from pythermalcomfort.utilities import clo_individual_garments
 # input variables
 tdb = 27  # dry bulb air temperature, [$^{\circ}$C]
 tr = 25  # mean radiant temperature, [$^{\circ}$C]
-v = 0.1  # average air speed, [m/s]
+v = 0.3  # average air speed, [m/s]
 rh = 50  # relative humidity, [%]
 activity = "Typing"  # participant's activity description
 garments = ["Sweatpants", "T-shirt", "Shoes or sandals"]
@@ -31,21 +31,16 @@ print(results)
 print(f"pmv={results['pmv']}, ppd={results['ppd']}%")
 
 # for users who wants to use the IP system
-results_ip = pmv_ppd(tdb=77, tr=77, vr=0.4, rh=50, met=met, clo=clo, units="IP")
+results_ip = pmv_ppd(tdb=77, tr=77, vr=0.6, rh=50, met=met, clo=clo, units="IP")
 print(results_ip)
 
 # If you want you can also pass an array of inputs
 from pythermalcomfort.models import pmv_ppd
-import pandas as pd
 from pythermalcomfort.utilities import v_relative, clo_dynamic
-import numpy as np
+import pandas as pd
 import os
 
 df = pd.read_csv(os.getcwd() + "/examples/template-SI.csv")
-
-import time
-
-start = time.time()
 
 ta = df["tdb"].values
 tr = df["tr"].values
@@ -54,18 +49,16 @@ rh = df["rh"].values
 met = df["met"].values
 clo = df["clo"].values
 
-v_rel = np.vectorize(v_relative)(vel, met)
-clo_d = np.vectorize(clo_dynamic)(clo, met)
+v_rel = v_relative(vel, met)
+clo_d = clo_dynamic(clo, met)
 results = pmv_ppd(ta, tr, v_rel, rh, met, clo_d, 0, "ashrae", "SI")
 
-# split the pmv column in two since currently contains both pmv and ppd values
-# results is an np.array and should be cast into a list before converted to a dataframe
-df_ = pd.DataFrame.from_dict(results)
-df = pd.concat([df, df_], axis=1, sort=False)
+df["vr"] = v_rel
+df["clo_d"] = clo_d
+df["pmv"] = results["pmv"]
+df["ppd"] = results["ppd"]
 
-end = time.time()
-
-print(end - start)
+print(df.head())
 
 # uncomment the following line if you want to save the data to .csv file
 # df.to_csv('results.csv')
@@ -78,8 +71,20 @@ import time
 iterations = 10000
 tdb = np.empty(iterations)
 tdb.fill(25)
+met = np.empty(iterations)
+met.fill(1.5)
 
+v_rel = v_relative(0.1, met)
+clo_d = clo_dynamic(1, met)
+
+# ASHRAE PMV
 start = time.time()
-pmv_ppd(tdb=tdb, tr=23, vr=0.3, rh=40, met=1.2, clo=1, standard="ashrae")
+pmv_ppd(tdb=tdb, tr=23, vr=v_rel, rh=40, met=1.2, clo=clo_d, standard="ashrae")
+end = time.time()
+print(end - start)
+
+# ISO PMV
+start = time.time()
+pmv_ppd(tdb=tdb, tr=23, vr=v_rel, rh=40, met=1.2, clo=clo_d, standard="iso")
 end = time.time()
 print(end - start)
