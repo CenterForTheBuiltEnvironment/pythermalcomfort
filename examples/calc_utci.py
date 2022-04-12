fb1c08313ba9885ca5da9d4a2408ff4e5b7502b5
Federico <federicotartarini@gmail.com>
2022-04-12 10:44:03 +0800

    docs: added more examples to the utci

--- examples/calc_utci.py
+++ examples/calc_utci.py
@@ -1,7 +1,13 @@
 from pythermalcomfort.models import utci
+import numpy as np
+import time
 
 utci_val = utci(tdb=29, tr=30, v=1, rh=60)
 print(utci_val)
+
+utci_val = utci(tdb=60, tr=30, v=1, rh=60)
+print(utci_val)
+print(utci(tdb=60, tr=30, v=1, rh=60, limit_inputs=False))
 
 utci_val = utci(tdb=77, tr=77, v=6.56168, rh=60, units="IP")
 print(utci_val)
@@ -23,3 +29,11 @@ utci_val = utci(
     return_stress_category=True,
 )
 print(utci_val)
+
+iterations = 100000
+tdb = np.empty(iterations)
+tdb.fill(25)
+start = time.time()
+utci_val = utci(tdb=tdb, tr=30, v=1, rh=60)
+end = time.time()
+print(end - start)
