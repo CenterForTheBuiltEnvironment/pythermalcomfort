05a783b405128492738b3bf1fdea23e94c81fe13
federico Tartarini <federicotartarini@gmail.com>
2023-09-15 09:36:41 +1000

    docs: read the docs documentation theme

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,7 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.todo",
     "sphinx.ext.viewcode",
+    "sphinx_rtd_theme",
 ]
 source_suffix = ".rst"
 master_doc = "index"
@@ -37,8 +38,7 @@ extlinks = {
 # on_rtd is whether we are on readthedocs.org
 on_rtd = os.environ.get("READTHEDOCS", None) == "True"
 
-if not on_rtd:  # only set the theme if we're building docs locally
-    html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_rtd_theme"
 
 html_use_smartypants = True
 html_last_updated_fmt = "%b %d, %Y"
