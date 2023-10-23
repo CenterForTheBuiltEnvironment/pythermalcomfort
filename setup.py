2a4ca1a89b38e675e8a1c2256ae4066cb8f0b14c
federico Tartarini <federicotartarini@gmail.com>
2023-10-23 12:37:46 +1100

    Bump version: 2.8.7 → 2.8.8

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*names, **kwargs):
 
 setup(
     name="pythermalcomfort",
-    version="2.8.7",
+    version="2.8.8",
     license="MIT",
     description=(
         "Package to calculate several thermal comfort indices (e.g. PMV, PPD, SET,"
--- src/pythermalcomfort/__init__.py
+++ src/pythermalcomfort/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "__version__ = '2.8.7'"
+__version__ = "__version__ = '2.8.8'"
 
 from pythermalcomfort.models import *
