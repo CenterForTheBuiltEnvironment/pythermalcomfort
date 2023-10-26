33f50c2ef6e766273dd09fdae74e54b045b23cf6
federico Tartarini <federicotartarini@gmail.com>
2023-10-26 11:26:57 +1100

    docs: added sys to define path

--- docs/conf.py
+++ docs/conf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("../.."))
 
 extensions = [
     "sphinx.ext.autodoc",
