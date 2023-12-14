a8e5b5960e79ebd8a4fcc39c383ca898387ac64b
federico Tartarini <federicotartarini@gmail.com>
2023-12-14 17:39:24 +1100

    build: requires Python 3.9 or higher

--- setup.py
+++ setup.py
@@ -60,7 +60,6 @@ setup(
         "Operating System :: Microsoft :: Windows",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
@@ -85,7 +84,7 @@ setup(
         "thermal environment",
         "built environment",
     ],
-    python_requires=">=3.8.0",
+    python_requires=">=3.9.0",
     install_requires=[
         "scipy",
         "numba",
