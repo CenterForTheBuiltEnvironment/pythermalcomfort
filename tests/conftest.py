07056961560bb2e40d82021fd83881e6efb9eaa8
Twin Gan <twin.c.gan@gmail.com>
2024-09-13 10:42:14 +1000

    update compare function and ashrae, throw exception when test fail

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,6 +11,7 @@ unit_test_data_prefix = "https://raw.githubusercontent.com/TwinGan/validation-da
 test_adaptive_en_url = unit_test_data_prefix + "ts_adaptive_en.json"
 test_adaptive_ashrae_url = unit_test_data_prefix + "ts_adaptive_ashrae.json"
 
+
 @pytest.fixture
 def retrieve_data():
     def _retrieve_data(url):
@@ -23,16 +24,26 @@ def retrieve_data():
         except requests.RequestException as e:
             print(f"Error fetching data from {url}: {e}")
         return None
+
     return _retrieve_data
+
 
 # Custom equal method
 ## Json null equal to np.nan
+## np.ndarry compare with list
 @pytest.fixture
 def is_equal():
     def compare(a, b):
-        if (a is None and np.isnan(b)) or (b is None and np.isnan(a)):
+        if isinstance(a, np.ndarray):
+            if not isinstance(b, np.ndarray):
+                b = np.array(b, dtype=float)
+            b = np.where(b == None, np.nan, b)  # Replace None with np.nan
+            # Return True if arrays are close enough, including handling of NaN values
+            return np.allclose(a, b, equal_nan=True)
+        elif (a is None and np.isnan(b)) or (b is None and np.isnan(a)):
             return True
-        return a == b
+        else:
+            return a == b
 
     return compare
 
--- tests/test_adaptive_ashrae.py
+++ tests/test_adaptive_ashrae.py
@@ -13,21 +13,11 @@ def test_adaptive_ashrae(get_adaptive_ashrae_url, retrieve_data, is_equal):
             inputs["tdb"], inputs["tr"], inputs["t_running_mean"], inputs["v"], units
         )
         for key in outputs:
-            # Check if the result[key] is a numpy array
-            if isinstance(result[key], np.ndarray):
-                numpy_outputs = np.array(outputs[key], dtype=float)
-                numpy_outputs = np.where(numpy_outputs == None, np.nan, numpy_outputs)
-                try:
-                    np.testing.assert_almost_equal(result[key], numpy_outputs)
-                except AssertionError as e:
-                    print(
-                        f"Assertion failed for {key}. Expected {numpy_outputs}, got {result[key]}, inputs={inputs}\nError: {str(e)}"
-                    )
-            else:
-                # Use the custom is_equal for other types
-                try:
-                    assert is_equal(result[key], outputs[key])
-                except AssertionError as e:
-                    print(
-                        f"Assertion failed for {key}. Expected {outputs[key]}, got {result[key]}, inputs={inputs}\nError: {str(e)}"
-                    )
+            # Use the custom is_equal for other types
+            try:
+                assert is_equal(result[key], outputs[key])
+            except AssertionError as e:
+                print(
+                    f"Assertion failed for {key}. Expected {outputs[key]}, got {result[key]}, inputs={inputs}\nError: {str(e)}"
+                )
+            raise
