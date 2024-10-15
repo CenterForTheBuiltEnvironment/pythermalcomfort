2d37dd162cf09a91f87034d36069127db25af3f2
federico tartarini <federicotartarini@gmail.com>
2024-10-15 17:35:01 +1100

    lint(conftest): removed repetitive code and re-wrote some tests

--- tests/test_a_pmv.py
+++ tests/test_a_pmv.py
@@ -1,11 +1,19 @@
+import pytest
 from pythermalcomfort.models import a_pmv
+from tests.conftest import Urls
 
 
-def test_a_pmv(get_a_pmv_url, retrieve_data, is_equal):
-    reference_table = retrieve_data(get_a_pmv_url)
+def test_a_pmv(get_test_url, retrieve_data, is_equal):
+    reference_table = retrieve_data(get_test_url(Urls.A_PMV.name))
+
+    if reference_table is None:
+        pytest.fail(
+            f"Failed to retrieve reference table for {Urls.A_PMV.value.lower()}"
+        )
+
     for entry in reference_table["data"]:
         inputs = entry["inputs"]
-        outputs = entry["outputs"]
+        expected_output = entry["outputs"]["a_pmv"]
         result = a_pmv(
             inputs["tdb"],
             inputs["tr"],
@@ -16,9 +24,9 @@ def test_a_pmv(get_a_pmv_url, retrieve_data, is_equal):
             inputs["a_coefficient"],
         )
         try:
-            assert is_equal(result, outputs["a_pmv"])
+            assert is_equal(result, expected_output)
         except AssertionError as e:
             print(
-                f"Assertion failed for a_pmv. Expected {outputs}, got {result}, inputs={inputs}\nError: {str(e)}"
+                f"Assertion failed for {Urls.A_PMV.value.lower()}. Expected {expected_output}, got {result}, inputs={inputs}\nError: {str(e)}"
             )
             raise
--- tests/test_adaptive_ashrae.py
+++ tests/test_adaptive_ashrae.py
@@ -1,10 +1,16 @@
-import numpy as np
-
+import pytest
 from pythermalcomfort.models import adaptive_ashrae
+from tests.conftest import Urls
 
 
-def test_adaptive_ashrae(get_adaptive_ashrae_url, retrieve_data, is_equal):
-    reference_table = retrieve_data(get_adaptive_ashrae_url)
+def test_adaptive_ashrae(get_test_url, retrieve_data, is_equal):
+    reference_table = retrieve_data(get_test_url(Urls.ADAPTIVE_ASHRAE.name))
+
+    if reference_table is None:
+        pytest.fail(
+            f"Failed to retrieve reference table for {Urls.ADAPTIVE_ASHRAE.value.lower()}"
+        )
+
     for entry in reference_table["data"]:
         inputs = entry["inputs"]
         outputs = entry["outputs"]
@@ -13,11 +19,10 @@ def test_adaptive_ashrae(get_adaptive_ashrae_url, retrieve_data, is_equal):
             inputs["tdb"], inputs["tr"], inputs["t_running_mean"], inputs["v"], units
         )
         for key in outputs:
-            # Use the custom is_equal for other types
             try:
                 assert is_equal(result[key], outputs[key])
             except AssertionError as e:
                 print(
-                    f"Assertion failed for {key}. Expected {outputs[key]}, got {result[key]}, inputs={inputs}\nError: {str(e)}"
+                    f"Assertion failed for {Urls.ADAPTIVE_ASHRAE.value.lower()}. Expected {outputs[key]}, got {result[key]}, inputs={inputs}\nError: {str(e)}"
                 )
                 raise
