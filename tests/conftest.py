4a067ab530095beb98d11b20206233fb689ba62c
Twin Gan <twin.c.gan@gmail.com>
2024-10-22 20:00:58 +1100

    update scripts_test_result

--- tests/conftest.py
+++ tests/conftest.py
@@ -74,7 +74,7 @@ def is_equal(a, b, tolerance=1e-6):
         if a.dtype.kind in "UOS":  # U = unicode, O = objects, S = string
             return np.array_equal(a, b)
         else:
-            b = np.where(b == None, np.nan, b)  # Replace None with np.nan
+            b = np.where(b is None, np.nan, b)  # Replace None with np.nan
             # Return True if arrays are close enough, including handling of NaN values
             return np.allclose(a, b, atol=tolerance, equal_nan=True)
     elif (a is None and np.isnan(b)) or (b is None and np.isnan(a)):
--- tests/scripts_test_result.py
+++ tests/scripts_test_result.py
@@ -4,11 +4,18 @@ import re
 import os
 import sys
 
+"""
+The `test-script.R` script is designed to leverage the `tox` command for running
+tests across various functions. It will automatically generate a markdown table
+to record all the test results. The markdown table will be printed on the terminal
+and add to the README.rst
+
+"""
 
 def run_tox(env):
     log_file_path = "./tox.log"
     try:
-        with open(log_file_path, "w") as log_file:
+        with open(log_file_path, "w", encoding="utf-8") as log_file:
             if env is None:
                 tox = ["tox"]
             else:
@@ -23,7 +30,7 @@ def run_tox(env):
 def parse_tox_log(log_path):
     results = {}
     try:
-        with open(log_path, "r") as file:
+        with open(log_path, "r", encoding="utf-8") as file:
             for line in file:
                 if "PASSED" in line:
                     func = re.search(r"/([^/]*?)::", line)
@@ -40,6 +47,27 @@ def create_markdown_table(df):
     return df.to_markdown(index=False)
 
 
+def add_markdown_table_to_readme(markdown_output):
+    try:
+        with open("./README.rst", "r", encoding="utf-8") as f:
+            readme_content = f.read()
+
+        pattern = "----------------------------\n"
+        if pattern in readme_content:
+            index = readme_content.index(pattern)
+            readme_content = readme_content[:index]
+
+        with open("./README.rst", "w", encoding="utf-8") as f:
+            f.writelines(readme_content)
+            f.write(pattern)
+            f.write(markdown_output)
+            f.write("\n")
+            f.write(pattern)
+
+    except FileNotFoundError:
+        print("README.rst not found.")
+
+
 if __name__ == "__main__":
     env = None
     if len(sys.argv) > 1:
@@ -49,4 +77,5 @@ if __name__ == "__main__":
     df = parse_tox_log("./tox.log")
     markdown_output = create_markdown_table(df)
     print(markdown_output)
+    add_markdown_table_to_readme(markdown_output)
     os.remove("./tox.log")
