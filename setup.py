d15f9eee05459da4084a3ca02562d96bcbf9500b
federico tartarini <federicotartarini@gmail.com>
2024-12-20 11:02:02 +1100

    Bump version: 2.10.1 → 2.11.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def read(*names, **kwargs):
 
 setup(
     name="pythermalcomfort",
-    version="2.10.1",
+    version="2.11.0",
     license="MIT",
     description=(
         "Package to calculate several thermal comfort indices (e.g. PMV, PPD, SET,"
