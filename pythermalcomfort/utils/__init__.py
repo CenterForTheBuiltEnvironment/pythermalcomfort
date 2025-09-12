80e5d1090c63c6e034099b86a72cfda88fcc5389
federico tartarini <federicotartarini@gmail.com>
2025-09-12 18:24:42 +1000

    refactor(__init__): Update import statements to relative paths
    Change absolute imports to relative imports for better module organization
    and to adhere to Python best practices.

--- pythermalcomfort/utils/__init__.py
+++ pythermalcomfort/utils/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from pythermalcomfort.utils.scale_wind_speed_log import scale_wind_speed_log
+from .scale_wind_speed_log import scale_wind_speed_log
 
 __all__ = [
     "scale_wind_speed_log",
