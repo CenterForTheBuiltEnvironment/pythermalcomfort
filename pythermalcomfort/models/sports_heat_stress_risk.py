5606a0319c9624eedb6ac195a5ef068bb5c7e1e7
kmar0531 <k.boychova1314@gmail.com>
2025-11-03 09:30:47 +1100

    feat: removed unused variable

--- pythermalcomfort/models/sports_heat_stress_risk.py
+++ pythermalcomfort/models/sports_heat_stress_risk.py
@@ -65,96 +65,96 @@ def sports_heat_stress_risk(
     sport: _SportsValues,
 ):
     """Short description. todo add the description form the paper.
-    This study evaluates Sports Medicine Australia's 2021 extreme heat policy using a heat-balance model, showing improved protection against heat stress for adults in community sports.
+     This study evaluates Sports Medicine Australia's 2021 extreme heat policy using a heat-balance model, showing improved protection against heat stress for adults in community sports.
 
-    Parameters
-    ----------
-    tdb : float or list of float
-        Dry bulb air temperature [°C].
-    tr : float or list of float
-        Mean radiant temperature [°C].
-    rh : float or list of float
-        Relative humidity [%].
-    vr : float or list of float
-        Relative air speed [m/s].
-    sport¨ : _SportsValues
-        Sport-specific activity dataclass with fields ``clo``, ``met``, ``vr``, and ``duration``.
-        Use one of the entries from :pyclass:`Sports`.
+     Parameters
+     ----------
+     tdb : float or list of float
+         Dry bulb air temperature [°C].
+     tr : float or list of float
+         Mean radiant temperature [°C].
+     rh : float or list of float
+         Relative humidity [%].
+     vr : float or list of float
+         Relative air speed [m/s].
+     sport¨ : _SportsValues
+         Sport-specific activity dataclass with fields ``clo``, ``met``, ``vr``, and ``duration``.
+         Use one of the entries from :pyclass:`Sports`.
 
-    Returns
-    -------
-    # todo this should be a dataclass
-    SportsHeatStressRiskResult
-        Dataclass containing the results:
-        - pmv : Predicted Mean Vote (float or np.ndarray)
-        - p : Heat strain probability (float or np.ndarray)
-        - risk_level : Risk classification (str or np.ndarray)
+     Returns
+     -------
+     # todo this should be a dataclass
+     SportsHeatStressRiskResult
+         Dataclass containing the results:
+         - pmv : Predicted Mean Vote (float or np.ndarray)
+         - p : Heat strain probability (float or np.ndarray)
+         - risk_level : Risk classification (str or np.ndarray)
 
-    Raises
-    ------
+     Raises
+     ------
 
-    ValueError
-        If the risk level could not be determined due to NaN thresholds or if the internal
-        solver fails to produce thresholds that allow a risk determination.
+     ValueError
+         If the risk level could not be determined due to NaN thresholds or if the internal
+         solver fails to produce thresholds that allow a risk determination.
 
-    Examples
-    --------
-    .. code-block:: python
+     Examples
+     --------
+     .. code-block:: python
 
-        from pythermalcomfort.models import pmv_ppd_iso
-        from pythermalcomfort.utilities import v_relative
+         from pythermalcomfort.models import pmv_ppd_iso
+         from pythermalcomfort.utilities import v_relative
 
-        tdb = 25
-        tr = 25
-        rh = 50
-        v = 0.1
+         tdb = 25
+         tr = 25
+         rh = 50
+         v = 0.1
 
-        from pythermalcomfort.models.sports_heat_stress_risk import (
-            sports_heat_stress_risk,
-            Sports,
-        )
+         from pythermalcomfort.models.sports_heat_stress_risk import (
+             sports_heat_stress_risk,
+             Sports,
+         )
 
-        # call with prepared variables
-        sports_heat_stress_risk(tdb=tdb, tr=tr, rh=rh, vr=v, sport=Sports.RUNNING)
-        # Expected: ~2.4
+         # call with prepared variables
+         sports_heat_stress_risk(tdb=tdb, tr=tr, rh=rh, vr=v, sport=Sports.RUNNING)
+         # Expected: ~2.4
 
-   # todo add references to the docstring
+    # todo add references to the docstring
 
-    References
-    ----------
-    1. Sports Medicine Australia. (2021). *Extreme Heat Policy for Community Sport*.
-       https://sma.org.au/resources-advice/extreme-heat-policy/
+     References
+     ----------
+     1. Sports Medicine Australia. (2021). *Extreme Heat Policy for Community Sport*.
+        https://sma.org.au/resources-advice/extreme-heat-policy/
 
-    # todo add examples to the docstring
-    Examples
-    --------
-    .. code-block:: python
+     # todo add examples to the docstring
+     Examples
+     --------
+     .. code-block:: python
 
-        import numpy as np
-        from pythermalcomfort.models.sports_heat_stress_risk import (
-        sports_heat_stress_risk,
-        Sports,
-        SportsHeatStressRiskResult
-        )
+         import numpy as np
+         from pythermalcomfort.models.sports_heat_stress_risk import (
+             sports_heat_stress_risk,
+             Sports,
+             SportsHeatStressRiskResult,
+         )
 
-        # Define environmental conditions
-        tdb = 25          # Dry bulb temperature [°C]
-        tr = 25           # Mean radiant temperature [°C]
-        rh = 50           # Relative humidity [%]
-        vr = 0.1          # Air velocity [m/s]
+         # Define environmental conditions
+         tdb = 25  # Dry bulb temperature [°C]
+         tr = 25  # Mean radiant temperature [°C]
+         rh = 50  # Relative humidity [%]
+         vr = 0.1  # Air velocity [m/s]
 
-        # Choose sport
-        sport = Sports.RUNNING
+         # Choose sport
+         sport = Sports.RUNNING
 
-        # Call the function
-        result: SportsHeatStressRiskResult = sports_heat_stress_risk(
-        tdb=tdb, tr=tr, rh=rh, vr=vr, sport=sport
-        )
+         # Call the function
+         result: SportsHeatStressRiskResult = sports_heat_stress_risk(
+             tdb=tdb, tr=tr, rh=rh, vr=vr, sport=sport
+         )
 
-        # Access individual results
-        print("Predicted Mean Vote (PMV):", result.pmv)
-        print("Heat strain probability:", result.p)
-        print("Risk level:", result.risk_level)
+         # Access individual results
+         print("Predicted Mean Vote (PMV):", result.pmv)
+         print("Heat strain probability:", result.p)
+         print("Risk level:", result.risk_level)
     """
     # todo the function should accept either float or arrays as all the other functions pythermalcomfort
 
@@ -278,7 +278,7 @@ def _calc_risk_single_value(tdb, tr, rh, vr, sport: _SportsValues) -> float:
         else np.nan
     )
 
-    risk_level = np.nan
+    # risk_level = np.nan
 
     # check if the thresholds are within the min and max limits defined above
     if t_medium > max_t_low:
